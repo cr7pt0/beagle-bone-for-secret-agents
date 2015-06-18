0d36295e25a5d54dbd7ef13ac8de1011c1a25591
root <root@beaglebone.(none)>
2015-06-18 19:51:03 +0000

    add

---
+++ ch4/keypad/keypad/.build/uno/Keypad/Keypad.d
@@ -0,0 +1,12 @@
+.build/uno/Keypad/Keypad.d .build/uno/Keypad/Keypad.o: lib/Keypad/Keypad.cpp lib/Keypad/Keypad.h \
+ /usr/share/arduino/libraries/Keypad/utility/Key.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
---
+++ ch4/keypad/keypad/.build/uno/Keypad/utility/Key.d
@@ -0,0 +1,12 @@
+.build/uno/Keypad/utility/Key.d .build/uno/Keypad/utility/Key.o: /usr/share/arduino/libraries/Keypad/utility/Key.cpp \
+ /usr/share/arduino/libraries/Keypad/utility/Key.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h
