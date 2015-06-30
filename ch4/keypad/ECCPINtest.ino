a67c4beb95a574108ca8ec5d1af00794f138dc49
cr7pt0 <cryptoface-15@yahoo.com>
2015-06-30 16:15:51 -0400

    Update ECCPINtest.ino

--- ch4/keypad/ECCPINtest.ino
+++ ch4/keypad/ECCPINtest.ino
@@ -49,37 +49,6 @@ char passcode[] = {'1', '2', '3', '4', '5'};
 /* flag to trigger when to go collect the code or not */
 bool collect_code = false;
 
-//The following is used to generate random seed, to be replaced by HRNG
-extern "C" {
-
-static int RNG(uint8_t *dest, unsigned size) {
-  // Use the least-significant bits from the ADC for an unconnected pin (or connected to a source of 
-  // random noise). This can take a long time to generate random data if the result of analogRead(0) 
-  // doesn't change very frequently.
-  while (size) {
-    uint8_t val = 0;
-    for (unsigned i = 0; i < 8; ++i) {
-      int init = analogRead(0);
-      int count = 0;
-      while (analogRead(0) == init) {
-        ++count;
-      }
-      
-      if (count == 0) {
-         val = (val << 1) | (init & 0x01);
-      } else {
-         val = (val << 1) | (count & 0x01);
-      }
-    }
-    *dest = val;
-    ++dest;
-    --size;
-  }
-  // NOTE: it would be a good idea to hash the resulting random data using SHA-256 or similar.
-  return 1;
-}
-
-}  // extern "C"
 
 
 /**
@@ -94,54 +63,7 @@ void setup ()
   Wire.onRequest (provide_code);
   Wire.onReceive (receiveEvent3);
   Serial.begin(19200);
-  keygen ();
 }
-
-void keygen() {
-  uint8_t private1[uECC_BYTES];
-  uint8_t private2[uECC_BYTES];
-  
-  uint8_t public1[uECC_BYTES * 2];
-  uint8_t public2[uECC_BYTES * 2];
-  
-  uint8_t secret1[uECC_BYTES];
-  uint8_t secret2[uECC_BYTES];
-  
-  unsigned long a = millis();
-  uECC_make_key(public1, private1);
-  unsigned long b = millis();
-  
-  Serial.print("Made key 1 in "); Serial.println(b-a);
-  a = millis();
-  uECC_make_key(public2, private2);
-  b = millis();
-  Serial.print("Made key 2 in "); Serial.println(b-a);
-
-  a = millis();
-  int r = uECC_shared_secret(public2, private1, secret1);
-  b = millis();
-  Serial.print("Shared secret 1 in "); Serial.println(b-a);
-  if (!r) {
-    Serial.print("shared_secret() failed (1)\n");
-    return;
-  }
-
-  a = millis();
-  r = uECC_shared_secret(public1, private2, secret2);
-  b = millis();
-  Serial.print("Shared secret 2 in "); Serial.println(b-a);
-  if (!r) {
-    Serial.print("shared_secret() failed (2)\n");
-    return;
-  }
-    
-  if (memcmp(secret1, secret2, sizeof(secret1)) != 0) {
-    Serial.print("Shared secrets are not identical!\n");
-  } else {
-    Serial.print("Shared secrets are identical\n");
-  }
-}
-
 
 /**
  * Briefly flash the LED
@@ -175,7 +97,7 @@ bool get_code (char * buf, const int len)
           buf[x] = keypad.waitForKey();
             Serial.println("PIN Entered");
             Serial.println(buf[x]);
-            Serial.println();
+          keygen ();
           flash ();
         }
       result = true;
@@ -215,5 +137,18 @@ void receiveEvent3(int bytes)
     }
 
   collect_code = true;
+
+}
+
+void keygen() {
+  uint8_t private1[uECC_BYTES];
+  uint8_t public1[uECC_BYTES * 2];
+
+  uECC_make_key(public1, private1);
+  
+  Serial.println("Public Key");
+  Serial.println(public1);
+  Serial.println("Private Key");
+  Serial.println(private1);
 
 }
